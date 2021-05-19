0ebf866254d1c6d4f573d9e3407d237ac37922f3
Fedor Indutny <fedor@indutny.com>
2021-05-19 10:17:28 -0700

    Do ref counting in ThreadsafeTrampoline

--- src/trampoline.rs
+++ src/trampoline.rs
@@ -8,6 +8,7 @@ pub(crate) type Callback = Box<dyn FnOnce(Env) + Send + 'static>;
 
 pub(crate) struct ThreadsafeTrampoline {
     tsfn: ThreadsafeFunction<Callback>,
+    ref_count: u32,
 }
 
 impl ThreadsafeTrampoline {
@@ -16,7 +17,10 @@ impl ThreadsafeTrampoline {
     pub(crate) fn new(env: Env) -> Self {
         let tsfn = unsafe { ThreadsafeFunction::new(env, Self::callback) };
 
-        Self { tsfn: tsfn }
+        Self {
+            tsfn: tsfn,
+            ref_count: 1,
+        }
     }
 
     /// Schedules a closure to execute on the JavaScript thread that created
@@ -41,7 +45,12 @@ impl ThreadsafeTrampoline {
 
     /// References a trampoline to prevent exiting the event loop until it has been dropped. (Default)
     /// Safety: `Env` must be valid for the current thread
+    /// _Not idempotent_
     pub(crate) fn reference(&mut self, env: Env) {
+        self.ref_count += 1;
+        if self.ref_count != 1 {
+            return;
+        }
         unsafe {
             self.tsfn.reference(env);
         }
@@ -49,7 +58,16 @@ impl ThreadsafeTrampoline {
 
     /// Unreferences a trampoline to allow exiting the event loop before it has been dropped.
     /// Safety: `Env` must be valid for the current thread
+    /// _Not idempotent_
     pub(crate) fn unref(&mut self, env: Env) {
+        if self.ref_count == 0 {
+            return;
+        }
+        self.ref_count -= 1;
+
+        if self.ref_count != 0 {
+            return;
+        }
         unsafe {
             self.tsfn.unref(env);
         }
